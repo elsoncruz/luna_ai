8174af59e0a179afa92fa520b406e34a7ce095e9
elsoncruz <128441481+elsoncruz@users.noreply.github.com>
2024-04-19 18:54:28 +0530

    Update yy.py

--- yy.py
+++ yy.py
@@ -79,7 +79,7 @@ st.markdown(
     .st-emotion-cache-usj992{
         background-color: transparent;
     }
-    [data-testid="stAppViewContainer"]{
+    [data-testid="stApp stAppEmbeddingId-sc6yfrzwihse st-emotion-cache-13k62yr erw9t6i1"]{
         background-image:url("../static/09.jpg");
         background-size:cover;
     }
