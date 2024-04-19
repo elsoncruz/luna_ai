37e7f6c581f67d07a11020ff4226845d38be093b
elsoncruz <128441481+elsoncruz@users.noreply.github.com>
2024-04-19 20:14:43 +0530

    Update yy.py

--- yy.py
+++ yy.py
@@ -80,7 +80,7 @@ st.markdown(
         background-color: transparent;
     }
     [data-testid="stAppViewContainer"]{
-        background-image:url("./static/Back.png");
+        background-image:url("https://th.bing.com/th/id/OIP.cKwPnrUETftcFhuKn9W2FwHaEo?w=1280&h=800&rs=1&pid=ImgDetMain");
         background-size:cover;
         
     }
